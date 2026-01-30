b8ecbc5092177ceb930c87b4ad4006c34aaef503
Madhumitha Harikarthikeyan <136561127+madhuh1210@users.noreply.github.com>
2026-01-30 11:41:48 +0530

    Create 287.Find The Duplicate Number.java

---
+++ 287.Find The Duplicate Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(seen.contains(nums[i])){
+                return nums[i];
+            }
+            seen.add(nums[i]);
+        }
+        return -1;
+
+
+    }
+}
