9e301b4c23e5f52bada10801a21e17d64320af5b
Madhumitha Harikarthikeyan <136561127+madhuh1210@users.noreply.github.com>
2026-01-30 11:58:52 +0530

    Create 258.Add Digits.java

---
+++ 258.Add Digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {      // until single digit
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10; // take last digit
+                num /= 10;       // remove last digit
+            }
+
+            num = sum;
+        }
+        return num;
+    }
+}
